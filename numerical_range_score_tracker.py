dc62733c938e76f463519cce92cc6f55943ab4f0
Carl <carlsebpogi123@gmail.com>
2024-10-28 21:02:24 +0800

    Initial pseudocode uploaded

---
+++ numerical_range_score_tracker.py
@@ -0,0 +1,5 @@
+# Establish ranges within which the inputs are to be recorded
+# Prompt the user to input a number from 1-50
+# Track the given inputs. Numbers that belong to their respective ranges should be accounted for. Add one score to the range if the input belongs to that range
+# In the occurence of an invalid input, the program will end requesting for user input and will proceed to display the ranges along their recorded scores
+# Throughout the program, try to incorporate different methods of tracking the numbers and scores
