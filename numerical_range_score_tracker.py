897c38e27468b84c84e9fca909ada0d27eb306f6
Carl <carlsebpogi123@gmail.com>
2024-10-30 11:46:56 +0800

    I just realized that the use of counting tracker was very common, so I've added more ranges that uses my own concept for emphasis

--- numerical_range_score_tracker.py
+++ numerical_range_score_tracker.py
@@ -7,13 +7,13 @@
 
 
 # Used to record the scores for its ranges
-score_tracker1 = score_tracker2 = 0
-score_tracker3_list = []
 # Defined lists to avoid errors when the range using .count() is empty
-score_list1, score_list2 = [], []
+score_list1, score_list2, score_list3 = [], [], []
+score_tracker4 = 0
+score_tracker5_list = []
 
 # Used to store the inputs for display at the last prompt
-input_tracker1, input_tracker2, input_tracker4, input_tracker5 = [], [], [], []
+input_tracker1, input_tracker2, input_tracker3, input_tracker4 = [], [], [], []
 
 print("Please take note that in the occurrence of an invalid input, the program will proceed to display the ranges")
 while True:
@@ -21,26 +21,28 @@ while True:
         # Asks the user for a numerical input within 1-50
         user_input = int(input("Please enter a number from 1-50, inclusive: "))
         if 1<= user_input <= 10:
-            # If the user_input is within this range, the program adds 1 to the score tracker assigned to this range
-            score_tracker1 += 1
-            input_tracker1.append(user_input)
-        elif 11<= user_input <= 20:
-            score_tracker2 += 1
-            input_tracker2.append(user_input)
-        elif 21<= user_input <= 30:
-            # If the user input is within range, the program stores it in the assigned list and count its elements using len() at the bottom 
-            score_tracker3_list.append(user_input)
-        elif 31<= user_input <= 40:
             # Edit: My new concept here is to change each elements in the original list then assign it to new defined list, then use .count() to record the score
-            input_tracker4.append(user_input)
+            input_tracker1.append(user_input)
             # This comprehension format is sourced from stackoverflow.com and was tailored by me accordingly. Link: https://stackoverflow.com/questions/7126916/perform-a-string-operation-for-every-element-in-a-python-list
-            edit_input_tracker4 = ["i" for num1 in input_tracker4]
-            score_list1 = edit_input_tracker4 
-        elif 41<= user_input <= 50:
-            input_tracker5.append(user_input)
+            edit_input_tracker1 = ["i" for num1 in input_tracker1]
+            score_list1 = edit_input_tracker1
+        elif 11<= user_input <= 20:
+            input_tracker2.append(user_input)
             # Used "i" for much simpler representation
-            edit_input_tracker5 = ["i" for num2 in input_tracker5]
-            score_list2 = edit_input_tracker5
+            edit_input_tracker2 = ["i" for num2 in input_tracker2]
+            score_list2 = edit_input_tracker2
+        elif 21<= user_input <= 30:
+            input_tracker3.append(user_input)
+            # Used "i" for much simpler representation
+            edit_input_tracker3 = ["i" for num3 in input_tracker3]
+            score_list3 = edit_input_tracker3
+        elif 31<= user_input <= 40:
+            # If the user_input is within this range, the program adds 1 to the score tracker assigned to this range
+            score_tracker4 += 1
+            input_tracker4.append(user_input) 
+        elif 41<= user_input <= 50:
+            # If the user input is within range, the program stores it in the assigned list and count its elements using len() at the bottom 
+            score_tracker5_list.append(user_input)
         else:
             print("An invalid input has been entered. The ranges are as follows:")
             break
@@ -48,10 +50,11 @@ while True:
         print("An invalid input has been entered. The ranges are as follows:")
         break
 
-score_tracker3 = len(score_tracker3_list)
 # the .count() counts how many 'i' where on the num_list. This is the endgame of my method in range 41-50
-score_tracker4 = score_list1.count("i")
-score_tracker5 = score_list2.count("i")
+score_tracker1 = score_list1.count("i")
+score_tracker2 = score_list2.count("i")
+score_tracker3 = score_list3.count("i")
+score_tracker5 = len(score_tracker5_list)
 print(f"1 - 10 = {score_tracker1}")
 print(f"11 - 20 = {score_tracker2}")
 print(f"21 - 30 = {score_tracker3}")
@@ -71,7 +74,7 @@ while True:
 
 print(f"1 - 10: {input_tracker1}")
 print(f"11 - 20: {input_tracker2}")
-print(f"21 - 30: {score_tracker3_list}")
+print(f"21 - 30: {input_tracker3}")
 print(f"31 - 40: {input_tracker4}")
-print(f"41 - 50: {input_tracker5}")
+print(f"41 - 50: {score_tracker5_list}")
 
